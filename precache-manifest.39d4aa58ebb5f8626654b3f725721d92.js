f3ce8f7ce2c08017d68b470dd0d3970f75208b81
lab_1_10 <lab_1_10@lab-1-10s-iMac.local>
2021-12-22 11:44:05 +0800

    Updates

---
+++ precache-manifest.39d4aa58ebb5f8626654b3f725721d92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3644ebcd18a5221de49",
+    "url": "/courses/static/css/main.26b0275b.chunk.css"
+  },
+  {
+    "revision": "e3644ebcd18a5221de49",
+    "url": "/courses/static/js/main.e3644ebc.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/courses/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "b0914f7499e540a17bff",
+    "url": "/courses/static/js/runtime~main.b0914f74.js"
+  },
+  {
+    "revision": "497194aceac42ea8a8c40547ad5254e5",
+    "url": "/courses/index.html"
+  }
+];
